5ad9cdcd460b5fecf7ef899a4a3cf8acd3b81ff5
nagaohiroki <testplayers0226@gmail.com>
2018-11-19 14:48:19 +0900

    add UnityProject

---
+++ UnityProject/Assets/Scripts/Generator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+	[SerializeField]
+	GameObject mBall;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetButtonDown("Fire1"))
+		{
+			Generate();
+		}
+	}
+	void Generate()
+	{
+		var ball = Instantiate(mBall);
+		// var rigid = ball.GetComponent<Rigidbody>();
+		ball.transform.position = transform.position;
+		ball.SetActive(true);
+	//	rigid.MovePosition(transform.position);
+	}
+}

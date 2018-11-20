9e2561379d0381a1fbb59adf82ae04e7f4b1a973
nagaohiroki <testplayers0226@gmail.com>
2018-11-21 08:21:49 +0900

    Magシステム追加

---
+++ UnityProject/Assets/Scripts/Box.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+public class Box : MonoBehaviour
+{
+	[SerializeField]
+	Rigidbody mRigidbody;
+	[SerializeField]
+	GameObject mCameraHandle;
+	void Update()
+	{
+		if(mRigidbody != null)
+		{
+			var v = Vector3.zero;
+			v.z = -Input.GetAxis("Horizontal");
+			mRigidbody.AddTorque(v, ForceMode.VelocityChange);
+		}
+		if(mCameraHandle != null)
+		{
+			mCameraHandle.transform.position = transform.position;
+		}
+	}
+	void OnCollisionEnter(Collision inColl)
+	{
+		if(inColl.gameObject.tag == "Mag")
+		{
+			inColl.transform.SetParent(transform);
+		}
+	}
+}
